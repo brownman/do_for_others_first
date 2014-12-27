bd4a43169fe63b073d8bcc7006d48927c6bdedc2
brownman <brownman556@gmail.com>
2014-12-27 17:09:04 +0200

    Update install_modules.sh

--- install_modules.sh
+++ install_modules.sh
@@ -2,7 +2,7 @@ test -v dir_root
 steps(){
     bash -e $dir_root/steps/user.sh
     bash -e $dir_root/steps/library.sh 
-    bash -e $dir_root/steps/service.sh
+  #  bash -e $dir_root/steps/service.sh
 }
 
 steps 
