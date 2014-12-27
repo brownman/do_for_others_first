14f6d725993dbba601b749aa06d7ba88c96f910c
brownman <brownman556@gmail.com>
2014-12-27 15:02:17 +0200

    Update notify_send.sh

--- test/first/notify_send.sh
+++ test/first/notify_send.sh
@@ -1,4 +1,7 @@
- which notify-send
+
+#target: make notification with notify-send available 
+#why: many of the framework sub-modules are making use of this command to comminucate to the gui-user
+which notify-send
 whereis notify-send
 notify-send hi
  
