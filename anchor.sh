9f5e2314114abfc9c7f909d556679cce39594f19
brownman <brownman556@gmail.com>
2014-12-27 18:54:55 +0200

    Update anchor.sh

--- anchor.sh
+++ anchor.sh
@@ -6,5 +6,19 @@ echo 1>&2 "[dir_root] $dir_root"
 source $dir_root/config.cfg
 $cmd_trap_err
 
+start(){
+chmod +x $dir_root/bin/fix_permission.sh
+}
+end(){
+  commander0 $cmd_start
+}
+steps(){
+  bin/fix_permission.sh
+  bin/submodules_show.sh
+  bin/install_apt.sh
+}
+
 cmd_start=${1:-}
-commander0 $cmd_start
+start
+steps
+end
