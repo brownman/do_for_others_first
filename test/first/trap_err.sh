a3d89c0ed0b6ccdb45ee6b8fbf0266d618046510
brownman <brownman556@gmail.com>
2014-12-27 16:11:53 +0200

    Update trap_err.sh

--- test/first/trap_err.sh
+++ test/first/trap_err.sh
@@ -3,6 +3,6 @@
 #test: bash -E
 #target: check if  subshell inherit traps (ERR)
 #goal: persistant trap ERR within all subshells of parent script: travis.sh
-commander trap
-commander type trap_err0
-commander some_errr_1
+commander0 trap
+commander0 type trap_err0
+commander0 some_errr_1
