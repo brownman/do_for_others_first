01b428b9a20381d04ba7f36b1121f4412fb0801c
brownman <brownman556@gmail.com>
2014-12-27 17:38:03 +0200

    Update user.sh

--- steps/user.sh
+++ steps/user.sh
@@ -22,7 +22,7 @@ test_after(){
    # local file=$HOME/.link
   #  test -f $HOME/.link
   #  cat $HOME/.bashrc | grep '.link'
-    (  set +u; source $HOME/.bashrc; )
+    ( unset dir_root; set +u; source $HOME/.bashrc; test -v dir_root; test $STEP -eq 1 )
 
 }
 
