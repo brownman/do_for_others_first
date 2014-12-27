c9106100f14cee834ec8570709464822a05d6bfe
brownman <brownman556@gmail.com>
2014-12-27 14:54:33 +0200

    tests: test all lists (txt)

--- test/test.sh
+++ test/test.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
+local dir=$dir_root/test/BANK
 
-source /tmp/library.cfg
 
-testing(){
-local file=$module/list.txt
-while read line;do
-  test -n "$line" || break
-  commander $line
+single(){
+  local file=$1
+  ensure0 test -s $file 
+  
+  while read line;do
+    test -n "$line" || { trace breaking; break; } 
+    commander0 $line
 done < $file
 }
 
+all(){
+local list=$( ls -1 $dir/*.txt )
+local file
+for item in $list;do
+  file="$dir/$item"
+  test -f $file && {  single $item; } || { trace0 "[ERR] file not found: $file"; }
+done
+}
+
 steps(){
-module=library
-commander testing
+all
 }
 
 steps
