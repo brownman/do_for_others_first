8d6108ff5112874af0b47fced07dfa07eda8661d
brownman <brownman556@gmail.com>
2014-12-27 15:04:28 +0200

    Create install_apt.sh

---
+++ bin/install_apt.sh
@@ -0,0 +1,23 @@
+apt1(){
+sudo apt-get install -y -q <<START
+libnotify-bin
+fortune-mod
+toilet
+curl
+wget
+gxmessage 
+tree
+vim-gtk
+pv
+cowsay
+toilet
+figlet
+sox
+libsox-fmt-mp3
+zenity
+xcowsay
+flite
+START
+}
+
+apt1
